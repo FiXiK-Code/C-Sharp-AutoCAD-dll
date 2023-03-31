8ad3272e49b801af820d34d17fce51d06a37ed8a
FiXiK-Code <tailedstory@gmail.com>
2023-04-01 02:09:40 +0500

    my version

--- Size_Separation_Point/CommandClass.cs
+++ Size_Separation_Point/CommandClass.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Interop;
 using Autodesk.AutoCAD.Interop.Common;
 using Autodesk.AutoCAD.Runtime;
@@ -48,40 +49,53 @@ namespace Size_Separation_Point
             }
 
 
-
-
-
-
             //рисование линии по двум точкам
-            AcadApplication acApp = (AcadApplication)Marshal.GetActiveObject("AutoCad.Application");
-
-            AcadDocument acaddoc = acApp.ActiveDocument;
-
-            AcadUtility acUtilit = acaddoc.Utility;
-
-            AcadModelSpace acModel = acaddoc.Database.ModelSpace;
-
-            acaddoc.ActiveSpace = AcActiveSpace.acModelSpace;
+            
 
             while (true)
             {
-                double[] pt1 = acUtilit.GetPoint(Prompt: "\nУкажите первую точку: ");
+                PromptPointResult pt1 = adoc.Editor.GetPoint("\nУкажите первую точку: ");
+                Point3d startPoint = pt1.Value;
+                if (pt1.Status == PromptStatus.Cancel) return;
+                
 
-                if (pt1 == null) return;
+                PromptPointResult pt2 = adoc.Editor.GetPoint("\nУкажите вторую точку: ");
+                Point3d endPoint = pt2.Value;
+                if (pt2.Status == PromptStatus.Cancel) return;
 
-                double[] pt2 = acUtilit.GetPoint(pt1, "\nУкажите вторую точку: ");
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                {
+                    BlockTable blockTable;
+                    BlockTableRecord blockTableRes;
 
-                if (pt2 == null) return;
 
-                double[] pt1wcs = acUtilit.TranslateCoordinates
-                    (pt1, AcCoordinateSystem.acUCS, AcCoordinateSystem.acWorld, 0);
+                    blockTable = tr.GetObject(db.BlockTableId, OpenMode.ForNotify) as BlockTable;
 
-                double[] pt2wcs = acUtilit.TranslateCoordinates
-                    (pt2, AcCoordinateSystem.acUCS, AcCoordinateSystem.acWorld, 0);
+                    blockTableRes = tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-                AcadLine acLine = acModel.AddLine(pt1wcs, pt2wcs);
 
-                acUtilit.Prompt("\nСоздан объект: " + acLine.Handle);
+                    using (Line acLine = new Line(startPoint,endPoint))
+                    {
+                        blockTableRes.AppendEntity(acLine);
+                        tr.AddNewlyCreatedDBObject(acLine, true);
+                    }
+
+                    PromptPointResult pt3 = adoc.Editor.GetPoint("\nУкажите вылет размера: ");
+                    Point3d ptDim = pt3.Value;
+                    if (pt3.Status == PromptStatus.Cancel) return;
+
+                    using (AlignedDimension newDim = new AlignedDimension(startPoint, endPoint, ptDim, null, default))
+                    {
+                        blockTableRes.AppendEntity(newDim);
+                        tr.AddNewlyCreatedDBObject(newDim, true);
+                    }
+
+
+
+                        tr.Commit();
+                }
+
+
             }
 
 
@@ -91,36 +105,91 @@ namespace Size_Separation_Point
         public void RunCommand2()
         {
             Document adoc = Application.DocumentManager.MdiActiveDocument;
-
             Database db = adoc.Database;
-
             Editor ed = adoc.Editor;
-
-            PromptEntityResult result = ed.GetEntity("\nВыберите размер: ");
-            if (result.Status != PromptStatus.OK) return;
-
-            ObjectId enId = result.ObjectId;
-
-            bool isCorrect = enId.IsValid
-                && !enId.IsErased
-                && enId.IsEffectivelyErased
-                && enId.ObjectClass.Name.Equals("AcDbAlignedDimension");
-            if (!isCorrect)
+            while (true)
             {
-                ed.WriteMessage("\nВыбран некорректный объект!");
-                return;
+                PromptEntityResult result = ed.GetEntity("\nВыберите размер: ");
+                if (result.Status == PromptStatus.Cancel)
+                {
+                    return;
+                }
+
+                ObjectId enId = result.ObjectId;
+
+                if (!enId.ObjectClass.Name.Equals("AcDbAlignedDimension"))
+                {
+                    ed.WriteMessage("\nВыбран объект: " + result.ObjectId.ObjectClass.Name);
+                    return;
+                }
+
+
+
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                {
+                    AlignedDimension obj = tr.GetObject(enId, OpenMode.ForRead, false, true) as AlignedDimension;
+                    BlockTable blockTable = tr.GetObject(db.BlockTableId, OpenMode.ForNotify) as BlockTable;
+                    BlockTableRecord blockTableRes = tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+
+
+                    Point3d startPoint = obj.XLine1Point;
+                    Point3d endPoint = obj.XLine2Point;
+                    Point3d dimPoint = obj.DimLinePoint;
+
+
+
+                    PromptPointResult pt1 = adoc.Editor.GetPoint("\nУкажите точку разделения: ");
+                    Point3d sepPoint = pt1.Value;
+                    ed.WriteMessage("\nString result: " + pt1.StringResult);
+                    if (pt1.Status == PromptStatus.Cancel) return;
+
+
+                    sepPoint = GetProjectionOnLine(sepPoint, startPoint, endPoint);
+                    if (sepPoint == new Point3d())
+                    {
+                        ed.WriteMessage("\nТочка за пределами отрезка!");
+                    }
+                    else
+                    {
+                        obj.UpgradeOpen();
+                        obj.Erase();
+
+                        using (AlignedDimension newDim = new AlignedDimension(startPoint, sepPoint, dimPoint, null, default))
+                        {
+                            blockTableRes.AppendEntity(newDim);
+                            tr.AddNewlyCreatedDBObject(newDim, true);
+                        }
+
+                        using (AlignedDimension newDim = new AlignedDimension(sepPoint, endPoint, dimPoint, null, default))
+                        {
+                            blockTableRes.AppendEntity(newDim);
+                            tr.AddNewlyCreatedDBObject(newDim, true);
+                        }
+                    }
+
+                    tr.Commit();
+                }
+            }
+            
+
+        }
+
+        public static Point3d GetProjectionOnLine(Point3d point, Point3d startPoint, Point3d endPoint)
+        {
+            var line = new Line(startPoint, endPoint);
+            var projectionOnStartToEnd = point - startPoint;
+            var startToEnd = endPoint - startPoint;
+            var direction = startToEnd.GetNormal();
+            var projectionOnDirection = projectionOnStartToEnd.DotProduct(direction) * direction;
+
+            if (startPoint.DistanceTo(startPoint + projectionOnDirection) >= startPoint.DistanceTo(endPoint)
+                || endPoint.DistanceTo(startPoint) <= endPoint.DistanceTo(startPoint + projectionOnDirection))
+            {
+                return new Point3d();
             }
 
-            using (Transaction tr = db.TransactionManager.StartTransaction())
-            {
-                DBObject obj = tr.GetObject(enId, OpenMode.ForRead, false, true);
-
-
-
-
-                tr.Commit();
-            }
-
+            return startPoint + projectionOnDirection;
         }
     }
 }
